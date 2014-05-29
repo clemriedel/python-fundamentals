b3ac8d57a3256d7c7aaf8dffe270530de50d3160
clem riedel <clement.riedel@gmail.com>
2014-05-29 14:39:35 -0700

    'test'

--- challenges/04-Functions/A_checking_again.py
+++ challenges/04-Functions/A_checking_again.py
@@ -5,10 +5,10 @@ sally_age = 12
 john_age = 22
 
 def can_drink(age):
- if age > 21: # conditionals take True or False
-    return True
- else:
-    return False
+	if age >= 21: # conditionals take True or False
+    		return True
+ 	else:
+    		return False
 
 
 
@@ -20,7 +20,18 @@ print "John can drink?", can_drink(john_age)
 
 def enforce(name, age, is_drinking):
     # Replace "pass" with some logic to test things out
-    pass
+    if  is_drinking == True:
+    	print "Checking ", name+"!", 
+    	can_drink(age)
+    	if can_drink(age) == True:
+    		print "OK"
+    	else:
+    		print "This isn't right!"
+    else:
+    	print name , "is not drinking"
+    		
+    
+    	
     # When you check,
     # print "Checking Name!"
     # (you'll need to set the name)
@@ -39,6 +50,18 @@ charles_age = 22
 charles_is_drinking = True
 
 enforce(alice_name, alice_age, alice_is_drinking)
+
+enforce(bob_name, bob_age, bob_is_drinking)
+
+enforce(charles_name, charles_age, charles_is_drinking)
+
+d = locals()
+individual = "bob"
+print individual+"_name"
+enforce(d[individual+"_name"], d[individual+"_age"], d[individual+"_is_drinking"])
+
+enforce("Julien",25,True)
+
 # And so on - do this for Bob and Charles!
 
 # That should've been a LOT less typing!
